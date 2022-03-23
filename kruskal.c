e882b1d8f11c42552381a02d9e2925dc062ca742
Jakub Kacálek <kacalek.kuba@post.cz>
2022-03-23 21:38:18 +0100

    draw a rectangle on click

--- kruskal.c
+++ kruskal.c
@@ -4,15 +4,59 @@
 #include <X11/Xlib.h>
 #include <string.h>
 
-
-
 int WINDOW_WIDTH = 600;
 int WINDOW_HEIGHT = 600;
+
+typedef struct node
+{
+    int x;
+    int y;
+    struct node *next;
+} node;
+
+node *create_node(int x, int y)
+{
+    node *new_node = malloc(sizeof(node));
+    new_node->x = x;
+    new_node->y = y;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void Append(node *head, node *odeasdf)
+{
+    node *tmp = head;
+    if (head == NULL)
+    {
+        head = odeasdf;
+        return;
+    }
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = odeasdf;
+    return;
+}
 
 int main(int argc, char **argv)
 {
 
-char* hello = "Hello world!";
+    struct node *head = NULL;
+
+    head = create_node(-1, -1);
+
+    // point1->next = point2;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     struct node *point = NULL;
+    //     point = create_node(i);
+    //     Append(head, point);
+    // }
+    //     printf("head.x = %i, point2.x = %i, point3.x = %i, point4.x = %i, point6.x = %i\n", head->x, head->next->x, head->next->next->x, head->next->next->next->x, head->next->next->next->next->next->x);
+
+    char *hello = "Hello world!";
 
     XEvent event;
     Window window;
@@ -35,9 +79,11 @@ char* hello = "Hello world!";
     }
     screen = DefaultScreen(display);
     window = XCreateSimpleWindow(display, RootWindow(display, screen), 100, 100, WINDOW_WIDTH, WINDOW_HEIGHT, 1, BlackPixel(display, screen), WhitePixel(display, screen));
+
+    // XSelectInput(display, window, );
     XMapWindow(display, window);
 
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
+    XSelectInput(display, window, ExposureMask | KeyPressMask | ButtonPressMask | ButtonReleaseMask);
     gc = XCreateGC(display, window, valuemask, &values);
 
     XSetLineAttributes(display, gc, 2, 0, 0, 0); // 2 = thickness
@@ -45,17 +91,45 @@ char* hello = "Hello world!";
     myFont = XLoadQueryFont(display, "-misc-fixed-medium-r-normal--9-90-75-75-c-60-iso10646-1");
     while (1)
     {
+
+        XAllowEvents(display, AsyncPointer, CurrentTime);
+
         XNextEvent(display, &event);
+        switch (event.type)
+        {
+        case KeyPress:
+        {
+            printf("Key pressed: %s\n", XKeysymToString(XKeycodeToKeysym(display, event.xkey.keycode, 0)));
+            break;
+        }
+        case ButtonPress:
+        {
+            if (event.xbutton.button == 1)
+            {
+                        event.xbutton.x,
+                        event.xbutton.y);
+
+                        struct node *point = NULL;
+                        point = create_node(event.xbutton.x, event.xbutton.y);
+                        Append(head, point);
+            }
+            break;
+        }
+        }
+        node *tmp = head;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+            XFillRectangle(display, window, gc, tmp->x, tmp->y, 10, 10);
+            // printf("x:%i\ty:%i\n", tmp->x, tmp->y);
+        }
 
         XDrawLine(display, window, gc, 30, 30, 60, 60);
-                XDrawString(display, window, gc, 60, 60, hello, strlen(hello)); 
-
+        XDrawString(display, window, gc, 60, 60, hello, strlen(hello));
     }
 
-
     XFreeGC(display, gc);
-XDestroyWindow(display, window);
-XCloseDisplay(display);
-
-return 0;
+    XDestroyWindow(display, window);
+    XCloseDisplay(display);
+    return 0;
 }
